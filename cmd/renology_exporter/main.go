44fb8e7ea05333c231b3a3ef2556eb65f448212e
Ryan Davis <ryan@lazy-electron.com>
2022-03-12 21:15:41 -0500

    shift details to `internal`

--- cmd/renology_exporter/main.go
+++ cmd/renology_exporter/main.go
@@ -2,31 +2,17 @@ package main
 
 import (
 	"context"
-	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/lazy-electron-consulting/renology-exporter/internal/start"
 )
 
-var logger = log.New(log.Writer(), "[main] ", log.Lmsgprefix|log.Flags())
-
 func main() {
-	logger.Println("starting server")
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGHUP, syscall.SIGABRT)
 	defer stop()
-	http.Handle("/metrics", promhttp.Handler())
-	srv := &http.Server{Addr: ":8080"}
-	defer srv.Close()
-	go func() {
-		defer logger.Println("server stopped")
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			logger.Printf("error serving %v\n", err)
-			stop()
-		}
-	}()
-	logger.Println("server started, Ctrl-C to exit")
-	<-ctx.Done()
+	start.Run(ctx, &start.Config{
+		Port: 8080,
+	})
 }
---
+++ internal/start/start.go
@@ -0,0 +1,46 @@
+package start
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var logger = log.New(log.Writer(), "[start] ", log.Lmsgprefix|log.Flags())
+
+// Config carries all the inputs needed to run the exporter
+type Config struct {
+	Port int
+	Path string
+}
+
+// Run starts the server and runs until the given context is canceled
+func Run(ctx context.Context, cfg *Config) error {
+	logger.Println("starting")
+
+	ctx, stop := context.WithCancel(ctx)
+	defer stop()
+	defer logger.Println("stopped")
+
+	return runHttp(ctx, cfg)
+}
+
+func runHttp(ctx context.Context, cfg *Config) error {
+	http.Handle("/metrics", promhttp.Handler())
+	srv := &http.Server{Addr: fmt.Sprintf(":%d", cfg.Port)}
+	go func() {
+		<-ctx.Done()
+		srv.Close()
+	}()
+
+	defer logger.Println("http server stopped")
+	logger.Printf("http server started on %v\n", srv.Addr)
+	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		logger.Printf("error serving %v\n", err)
+		return err
+	}
+	return nil
+}
