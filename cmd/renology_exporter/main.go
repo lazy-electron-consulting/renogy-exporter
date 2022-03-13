ea2210b66b59afe849185d6db36eeed70f124330
Ryan Davis <ryan@lazy-electron.com>
2022-03-12 21:04:13 -0500

    add basic logging

--- cmd/renology_exporter/main.go
+++ cmd/renology_exporter/main.go
@@ -11,18 +11,22 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
+var logger = log.New(log.Writer(), "[main] ", log.Lmsgprefix|log.Flags())
+
 func main() {
-	log.Println("starting server")
+	logger.Println("starting server")
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGHUP, syscall.SIGABRT)
 	defer stop()
 	http.Handle("/metrics", promhttp.Handler())
 	srv := &http.Server{Addr: ":8080"}
 	defer srv.Close()
 	go func() {
+		defer logger.Println("server stopped")
 		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			logger.Printf("error serving %v\n", err)
 			stop()
 		}
 	}()
-	log.Println("server started, Ctrl-C to exit")
+	logger.Println("server started, Ctrl-C to exit")
 	<-ctx.Done()
 }
